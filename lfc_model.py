49c1b30822bbdd147df0e170ce3804fe6dae7c2e
RCNathan <nathan.vanderstrate@gmail.com>
2024-07-24 23:04:59 +0200

    implemented yan et al.'s 3-area model in lfc_model.py (though all areas are identical rn)

--- lfc_model.py
+++ lfc_model.py
@@ -8,33 +8,50 @@ import numpy as np
 class Model():
     """Class to store model information for the system."""
 
-    n: ClassVar[int] = 1  # number of agents
+    n: ClassVar[int] = 3  # number of agents
     nx_l: ClassVar[int] = 4  # local state dimension
     nu_l: ClassVar[int] = 1  # local control dimension
+
+    # constants (to be moved later maybe? Also: differences between areas)
+    # taken from Yan et al. three area network (for now, area 1)
+    Tg = 0.10 # governor time constant
+    Tt = 0.40 # turbine time constant
+    H = 0.0833 # synchronous machine inertia
+    D = 0.0015 # damping coefficient
+    R = 0.33 # speed drop
+    Tij = 0.015 # T12
 
     # note: unchanged as of 24-7
     x_bnd_l: ClassVar[np.ndarray] = np.array(
         [[0, -1], [1, 1]]
-    )  # local state bounds x_bnd[0] <= x <= x_bnd[1]
+    )  # local state bounds x_bnd[0] <= x <= x_bnd[1] 
     u_bnd_l: ClassVar[np.ndarray] = np.array(
         [[-1], [1]]
     )  # local control bounds u_bnd[0] <= u <= u_bnd[1]
 
-    
+    # adj is atm set up for 3 agents, so I guess I keep it at 3 agents for now.
     adj: ClassVar[np.ndarray] = np.array(
         [[0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=np.int32 #  [1, 0, 1]: connected to 1st and 3rd.
     )  # adjacency matrix of coupling in network
 
+    # TRUE/REAL (unknown) dynamics
+    # Note: this means dynamics are the same for each area (right now) 
     A_l: ClassVar[np.ndarray] = np.array(
-        [[0.9, 0.35], [0, 1.1]]
+        [[-D/(2*H), 1/(2*H), 0, -1/(2*H)], 
+         [0, -1/Tt, 1/Tt, 0], 
+         [-1/(R*Tg), 0, -1/Tg, 0], 
+         [np.inf, 0, 0, 0]]
     )  # local state-space matrix A
+    A_l[3,0] = 2 * np.pi * n * Tij # to be replaced by actual different Tij's
     B_l: ClassVar[np.ndarray] = np.array(
-        [[0.0813], [0.2]]
+        [[0], [0], [1/Tg], [0]]
     )  # local state-space matrix B
     A_c_l: ClassVar[np.ndarray] = np.array(
-        [[0, 0], [0, -0.1]]
+        np.zeros((4,4))
     )  # local coupling matrix A_c
+    A_c_l[3,0] = -2 * np.pi * n * Tij # to be replaced with sum T_ij
 
+    # starting point (inaccurate guess) for learning (excluding learnable params)
     A_l_innacurate: ClassVar[np.ndarray] = np.asarray(
         [[1, 0.25], [0, 1]]
     )  # inaccurate local state-space matrix A
@@ -123,5 +140,9 @@ class Model():
 
 
 m = Model()
+print("Local A matrix for one agent/area: \n", m.A_l)
+print("Local B matrix for one agent/area:", m.B_l)
+print("Local A_{ij} matrix for one agent/area: \n", m.A_c_l)
+
 
 print("dot is for debugging :)")
