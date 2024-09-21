c69d68cb74e8e36d562d698d1283441a580fdf29
nvanderstrate <nvanderstrate@student.tudelft.nl>
2024-09-21 19:05:51 +0200

    test.py for Joel

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
-# from lfc_model import Model # NOTE: on importing, all code in module is executed, and an instance is created (always).
-# print("test")
+from casadi import *
+from lfc_model import Model # NOTE: on importing, all code in module is executed, and an instance is created (always).
+print("test")
 
 
 N = 6
@@ -31,8 +32,8 @@ print(graph.adj)
 print("the end lol")
 
 
-from casadi import *
-x = MX("x") # creates 1-by-1 matrix with symbolic primitive called x
+
+x = MX.sym("x") # creates 1-by-1 matrix with symbolic primitive called x
 
 # create vector- or matrix-valued symbolic variables by supplying additional arguments to SX.sym:
 y = SX.sym('y',5) # creates vector 5 by 1
@@ -42,3 +43,32 @@ Z = SX.sym('Z',4,2) # creates matrix 4 by 2 (indexes columnwise)
 f = x**2 + 10
 f = sqrt(f)
 print(f)
+
+import numpy as np
+
+positions = np.array([
+    # x,  y,   z
+    [0.1, 0.2, 0.3], 
+    [0.0, 0.1, 0.2], 
+    [0.0, 0.4, 0.2], 
+    [0.1, 0.2, 0.3],
+    [0.1, 0.2, 0.3],
+    [0.1, 0.2, 0.3],
+    [0.1, 0.2, 0.3],    
+]) # shape = (7,3)
+masses = np.array([0, 1, 2, 3, 4, 5, 6]) # shape (7,)
+
+def CoM(masses, positions):
+    totalMass = 0
+    totalFr = 0 
+    if positions.shape[0] == masses.shape[0]: # checks if shapes match
+        for i in range(positions.shape[0]):
+            totalFr += masses[i]*positions[i, :] # ith row, all columns (i = all)
+            totalMass += masses[i] # a += b         <==>    a = a + b 
+        CoM = totalFr / totalMass
+    else:
+        print("Shapes do not match")
+        CoM = 0    
+    return CoM
+
+print("Center of Mass is", CoM(masses, positions))
